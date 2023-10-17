3738e96df1a539f3aeb14dbea27c2152e0344a4b
Jakub Bujak <jakbuj3@gmail.com>
2023-10-17 12:24:27 +0200

    adding first lecture

---
+++ 01-CourseIntroduction/hello.java
@@ -0,0 +1,16 @@
+
+/**
+ * hello
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        String name = "Jakub";
+        String surname = "Bujak";
+        int Age = 20;
+        boolean Year1 = false;
+        String field_of_study = "Informatyka stosowana";
+        String university = "Uniwersytet Ekonomiczny w Krakowie kurwa mac";   
+        System.out.println("My name is " + name + " and my surname is " + surname + " and my age is " + Age + " information that im 1 year studying is " + Year1 + " . Im studying " + field_of_study + " in " + university);            
+    }
+}
